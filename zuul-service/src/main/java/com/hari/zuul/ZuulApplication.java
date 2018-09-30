461e42b5e51b4d10f9c3f019b887b3959c66f575
Hari Babu <haribabupacharla@gmail.com>
2018-09-30 20:29:55 +0530

    "Zuul: Zuul integrated on the  port 8093"

---
+++ zuul-service/src/main/java/com/hari/zuul/ZuulApplication.java
@@ -0,0 +1,41 @@
+package com.hari.zuul;
+
+import com.hari.zuul.filter.ErrorFilter;
+import com.hari.zuul.filter.PostFilter;
+import com.hari.zuul.filter.PreFilter;
+import com.hari.zuul.filter.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
